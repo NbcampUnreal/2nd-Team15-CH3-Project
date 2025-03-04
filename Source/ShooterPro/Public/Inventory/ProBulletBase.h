3eeceae1e42b5022f56a2b4712e785fa349a2664
ByteFries <sjhun1999@naver.com>
2025-03-04 21:13:43 +0900

    이펙트 생성 중

--- Source/ShooterPro/Public/Inventory/ProBulletBase.h
+++ Source/ShooterPro/Public/Inventory/ProBulletBase.h
@@ -1,9 +1,8 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "GameplayCueNotify_Burst.h"
 #include "ProBulletBase.generated.h"
 
 class UGameplayEffect;
